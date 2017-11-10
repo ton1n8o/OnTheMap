99abdaaaa48442a0e64d80409bdc76f5779a9741
Antonio Carlos <antoniocarlos.dev@gmail.com>
2017-11-10 09:45:19 +0800

    Constants
    - creating Constants.swift
    - basic setup for the login view

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  OnTheMap
+//
+//  Created by Antonio on 11/10/17.
+//  Copyright © 2017 Antonio. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Udacity {
+        static let APIScheme = "https"
+        static let APIHost = "www.udacity.com"
+        static let APIPath = "/api"
+    }
+    
+    struct HTTPHeaderField {
+        static let accept = "Accept"
+        static let contentType = "Content-Type"
+    }
+    
+    struct HTTPHeaderFieldValue {
+        static let json = "application/json"
+    }
+}
